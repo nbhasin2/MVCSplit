e40a2b10602d924042665cec6148835329998159
Nishant Bhasin <nikieme3@gmail.com>
2020-03-12 02:13:30 -0400

    Initial Commit

---
+++ MVCSplit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVCSplit
+//
+//  Created by Nishant Bhasin on 2020-03-12.
+//  Copyright © 2020 Nishant Bhasin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
